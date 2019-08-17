4fbb58d1f045de5193703a3c864861fd07b50a36
Network Data Analysis Group <54192944+ndag@users.noreply.github.com>
2019-08-17 19:56:54 -0400

    added new quotient code via FPS type of ideas

---
+++ quotientUMFPS.m
@@ -0,0 +1,24 @@
+function uXt = quotientUMFPS(uX,t)
+
+% implement a Farthest Point Sampling type of algorithm
+% this is faster than using quotientUM.m which does it via Matlab's cluster
+% function
+
+uXt_big = uX;
+uXt_big(uX<t) = 0;
+
+I = [1];
+dI = uXt_big(1,:);
+
+for k=1:length(uX)
+    mx = max(dI);
+    if mx == 0
+        break
+    end
+    J = find(dI == mx);
+    I = [I, J(1)];
+    dI = min([dI;uXt_big(J(1),:)],[],1);
+end
+
+
+uXt = uX(I,I);
